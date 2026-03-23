1d3a9482ac5e68585f082debf77c360d4016650a
Meowkewok <ezra.lederman@mjbha.org>
2026-03-23 13:18:23 -0400

    final

--- src/App.java
+++ src/App.java
@@ -2,8 +2,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Substitutes substitutes = new Substitutes();
-        GUI.makeGUI(substitutes);
+        // Substitutes substitutes = new Substitutes();
+        // GUI.makeGUI(substitutes);
 
         // Uncomment to use CLI version 
         // Substitutes substitutes = new Substitutes("Recipes.txt");
